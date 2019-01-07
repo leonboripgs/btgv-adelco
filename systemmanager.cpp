9e3f09fd9a87be349fb2f240f6cc6e9d41c29b32
leonboripgs <leonboripgs0818@outlook.com>
2019-01-07 21:27:14 +1000

    first commit

---
+++ systemmanager.cpp
@@ -0,0 +1,6 @@
+#include "systemmanager.h"
+
+SystemManager::SystemManager(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ systemmanager.h
@@ -0,0 +1,16 @@
+#ifndef SYSTEMMANAGER_H
+#define SYSTEMMANAGER_H
+
+
+class SystemManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SystemManager(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // SYSTEMMANAGER_H
